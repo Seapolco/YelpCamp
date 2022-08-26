7d50e8950060c4de6d2d7d55c49bc80b5708fc45
Seapolco <seapolco@gmail.com>
2022-08-26 11:49:59 +0100

    S41:427

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -14,6 +15,7 @@ db.once("open", () => {
 
 
 const app = express();
+app.engine('ejs', ejsMate);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, 'views'));
@@ -35,6 +37,7 @@ app.get('/campgrounds', async (req,res) => {
 });
 
 app.get('/campgrounds/new', (req,res) => {
+    console.log(req.url)
     res.render('campgrounds/new')
 })
 
